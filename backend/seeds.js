8612ea7e544dbbce884d28b5ea26847961e4dd0d
royhas <royhas@yad2.co.il>
2022-04-19 22:11:17 +0300

    add seed file

---
+++ backend/seeds.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+require("./models/User");
+require("./models/Item");
+var Item = mongoose.model('Item');
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set("debug", true);
+
+
+const seedItems = () => {
+    const itemsSeed = [];
+    let i = 0
+    for (i; i < 120; i++) {
+        itemsSeed.push({
+            title: `How to train your ${i} dragon`,
+            description:"Ever wonder how?",
+            body: "Very carefully.",
+            tagList:["dragons","training"]
+        })
+    }
+    return itemsSeed
+}
+const seedDB = async () => {
+    await Item.deleteMany({});
+    await Item.insertMany(seedItems());
+}
+
+seedDB()
